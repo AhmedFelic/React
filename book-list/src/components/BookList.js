b17992407ad6963710e9aec659c57a8368ae5313
AhmedFelic <ahmedfelic1@gmail.com>
2023-01-16 23:35:43 +0100

    Finished

--- book-list/src/components/BookList.js
+++ book-list/src/components/BookList.js
@@ -8,37 +8,36 @@ import { useNavigate } from "react-router-dom"
 function BookList() {
 
     const [bookData, setBookData] = useState([])
-    const [searchTitle, setSearchTitle] = useState("")
+    const [query, setQuery] = useState("")
     const [currentPage, setCurrentPage] = useState(1)
     const [postsPerPage, setPostPerPage] = useState(5)
     const navigate = useNavigate();
    
     const getData = async () => {
         const response = await fetch(
-            "https://example-data.draftbit.com/books?_limit=50"
+            `https://example-data.draftbit.com/books?q=${query}&_limit=50`
         ).then((response) => response.json())
             .catch(err => console.log(err))
         setBookData(response)
-        console.log(response)
+        
     }
 
-
     useEffect(() => {
-        getData()
-    }, []);
+       if(query.length === 0 || query.length > 2)   getData()
+      
+    }, [query]);
+    
     const lastPostIndex = currentPage * postsPerPage;   // definisano, current page = 2, postPerPage = 5 = 10 zadnja.
     const firstPostIndex = lastPostIndex - postsPerPage; // zadnja - po str = 5
-    const currentPosts = bookData.slice(firstPostIndex, lastPostIndex)
-  
-    
-
+    const currentPosts =  bookData.slice(firstPostIndex, lastPostIndex)
+ 
     return (
         <>
             <Nav />
           
             <div className="books-main">
                 <span className="book-input-parent">
-                <input className="book-input"  placeholder="Enter book name... " onChange={(e) => setSearchTitle(e.target.value)}></input>
+                <input className="book-input"  placeholder="Enter book name... " onChange={(e) => setQuery(e.target.value)}></input>
                 </span>
                 <Pagination
                   totalPosts={bookData.length}
@@ -47,17 +46,8 @@ function BookList() {
                   currentPage={currentPage}
             />  <span className="pagination-number">{currentPage} - {bookData.length/postsPerPage}</span>
                 {
-                    currentPosts.filter((value) => {
-                        if (searchTitle === " ") {
-                            return value;
-                        } else if (
-                            
-                            value.title.toLowerCase().includes(searchTitle.toLowerCase())) {
-                            return value;
-                        }
-
-                    })
-                        .map((book) => (
+                    
+                    currentPosts.map((book) => (
                             <div className="books-container" key={book.id}>
                                 <img className="book-image" src={book.image_url}></img>
                                 <p className="book-title" title={book.title}>{book.title}  </p>
--- book-list/src/components/Nav.js
+++ book-list/src/components/Nav.js
@@ -5,7 +5,7 @@ function Nav (){
     return(
         <>
         <h2 className="nav-title">This is a website which displays books pulled from draftbit book data API</h2>
-        
+        <p>Search will fire after first 2 letters are inserted</p>
         </>
     )
 }

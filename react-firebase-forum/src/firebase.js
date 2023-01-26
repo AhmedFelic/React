86608d7b965a5cdec4878a24d8094ecb2321e51a
AhmedFelic <ahmedfelic1@gmail.com>
2023-01-26 19:54:18 +0100

    firebase setup

---
+++ react-firebase-forum/src/firebase.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBFfPTOjhYUm-uHggftpx9kNn2RlHMK2LQ",
+
+  authDomain: "forum-15e85.firebaseapp.com",
+
+  projectId: "forum-15e85",
+
+  storageBucket: "forum-15e85.appspot.com",
+
+  messagingSenderId: "191278384823",
+
+  appId: "1:191278384823:web:b5828271a9344d6472ef32"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
